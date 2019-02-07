f907347ba61d858f0604a2e365c4682e00264db5
CemeoCMO <cristianmo@alumnos.iesgalileo.es>
2019-02-07 10:16:56 +0100

    Listado de pacientes

--- clases/miHospital.php
+++ clases/miHospital.php
@@ -8,6 +8,25 @@ class miHospital
 		$this->conex = miConexion::conexion_instancia();
     }
 
+    public function comprobarDoctor($nif, $clave){
+        try {
+            $sql="SELECT * FROM `doctores` where DNI=? and PASSWORD=?";
+            $consulta=$this->conex->prepare($sql);
+            $consulta->bindParam(1, $nif);
+            $consulta->bindParam(2, $clave);
+            $consulta->execute();
+            if ($consulta->rowCount()>0){
+                $_SESSION["dni"]=$nif;
+                header("Location: ./bienvenida.php");
+            } else {
+                echo "<p class='error'>El usuario o la contraseña introducido/a no es válida.</p>";
+            }
+        } catch (PDOException $e){
+            echo "<p>Consulta: <b>" . $sql . "</b></p>";
+            echo "<p class='error'>Error: " . $e->getMessage() . "</p>";
+        }
+    }
+
 
     public function registrarDoctor($dni, $password, $nombre, $apellidos, $correo, $direccion, $cpostal, $ciudad, $provincia) {
         try {
@@ -34,5 +53,60 @@ class miHospital
 			echo "<p class='error'>Error: " . $e->getMessage() . "</p>";
         }
     }
+
+    public function listarPacientes($dniDoctor){
+        try{
+            $sql="SELECT DNI, NOMBRE, APELLIDOS, CORREO, DIRECCION, CPOSTAL, CIUDAD, PROVINCIA, EXPEDIENTE, SEVERIDAD FROM pacientes WHERE DNI_DOCTOR=?";
+            $consulta=$this->conex->prepare($sql);
+            $consulta->bindParam(1, $dniDoctor);
+            $consulta->execute();
+            if ($consulta->rowCount() > 0) {
+                echo "<table id='listar_table'>";
+                echo "<thead>";
+                echo "<tr>";
+                echo "<th>Dni</th>";
+                echo "<th>Nombre</th>";
+                echo "<th>Apellidos</th>";
+                echo "<th>Correo</th>";
+                echo "<th>Dirección</th>";
+                echo "<th>C. Postal</th>";
+                echo "<th>Ciudad</th>";
+                echo "<th>Provincia</th>";
+                echo "<th>Expedinete</th>";
+                echo "<th>Severidad</th>";
+                echo "<th>Borrar</th>";
+                echo "</tr>";
+                echo "</thead>";
+                echo "<tbody>";
+                while ($fila = $consulta->fetch(PDO::FETCH_ASSOC)){
+                    $dni="";
+                    echo "<tr>";
+                        foreach ($fila as $clave => $valor) {
+                            if ($clave=="DNI"){
+                                $dni=$valor;
+                                echo "<td>";
+                                echo "<a href='mostrarPaciente.php?dni=" . $valor . "'>" . $valor . "</a>";
+                                echo "</td>";
+                            } else {
+                                echo "<td>" . $valor . "</td>";
+                            }
+                            
+                        }
+                    echo "<td><input type='checkbox' name='borrar[]' value='" . $dni . "'></td>";
+                    echo "</tr>";
+                }
+                echo "</tbody>";
+                echo "</table>";
+            } else {
+                echo "<p>No tiene ningún paciente.</p>";
+            }
+        } catch (PDOException $e){
+            echo "<p>Consulta: <b>" . $sql . "</b></p>";
+            echo "<p class='error'>Error: " . $e->getMessage() . "</p>";
+        }
+
+    }
+
+    
 }
 ?>
