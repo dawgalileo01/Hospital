a108869453298fba7b020d0a3d55725ee6719bfc
dawgalileo01 <jesusgg@alumnos.iesgalileo.es>
2019-02-07 10:09:06 +0100

    Alta de paciente

---
+++ alta.php
@@ -0,0 +1,112 @@
+<?php
+    include('./clases/comprobar.inc.php');
+    require_once("./clases/miConexion.php");
+    require_once("./clases/miHospital.php");
+    session_name("hospital");
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    if (isset($_REQUEST["expediente"])) {
+        $error = false;
+
+        if (validarNif($_REQUEST["dni"]) === 0) {
+            echo "<p class='error'>El formato del dni no es correcto.</p>";
+            $error = true;
+        } else if (validarNif($_REQUEST["dni"]) === -1) {
+            echo "<p class='error'>La letra del dni no es correcta.</p>";
+            $error = true;
+        }
+
+        if (validarEmail($_REQUEST["correo"]) === -1) {
+            echo "<p class='error'>El formato del correo no es correcto.</p>";
+            $error = true;
+        }
+
+        if(!$error) {
+
+?>
+    <form action="#" method="post">
+        <p>
+            <label>Descripcion:</label>
+            <textarea name="descripcion" cols="30" rows="5" required="required"></textarea>
+        </p>
+        <p>
+            <label>Severidad:</label>
+            <select name="severidad">
+                <option value="Alta">Alta</option>
+                <option value="Media">Media</option>
+                <option value="Baja">Baja</option>
+            </select>
+        </p>
+        <p>
+            <input type="submit" value="Alta" name="alta">
+        </p>
+    </form>
+<?php
+        $_SESSION["dniPaciente"] = $_REQUEST["dni"];
+        $_SESSION["nombrePaciente"] = $_REQUEST["nombre"];
+        $_SESSION["apellidosPaciente"] = $_REQUEST["apellidos"];
+        $_SESSION["correoPaciente"] = $_REQUEST["correo"];
+        $_SESSION["direccionPaciente"] = $_REQUEST["direccion"];
+        $_SESSION["cpostalPaciente"] = $_REQUEST["cpostal"];
+        $_SESSION["ciudadPaciente"] = $_REQUEST["ciudad"];
+        $_SESSION["provinciaPaciente"] = $_REQUEST["provincia"];
+        } else {
+            echo "<a href='alta.php'>Volver</a>";
+        }
+    } else if (isset($_REQUEST["alta"])) {
+        $miHospital = new miHospital();
+        $miHospital->altaPaciente($_SESSION["dniPaciente"], $_SESSION["nombrePaciente"], $_SESSION["apellidosPaciente"], $_SESSION["correoPaciente"], $_SESSION["direccionPaciente"], $_SESSION["cpostalPaciente"], $_SESSION["ciudadPaciente"], $_SESSION["provinciaPaciente"], $_REQUEST["descripcion"], $_REQUEST["severidad"], $_SESSION["dni"]);
+    } else {
+?>
+    <form action="#" method="post">
+        <p>
+            <label>DNI (*):</label>
+            <input type="text" name="dni" required="required">
+        </p>
+        <p>
+            <label>Nombre (*):</label>
+            <input type="text" name="nombre" required="required">
+        </p>
+        <p>
+            <label>Apellidos (*):</label>
+            <input type="text" name="apellidos" required="required">
+        </p>
+        <p>
+            <label>Correo (*):</label>
+            <input type="text" name="correo" required="required">
+        </p>
+        <p>
+            <label>Direccion (*):</label>
+            <input type="text" name="direccion" required="required">
+        </p>
+        <p>
+            <label>Codigo Postal (*):</label>
+            <input type="text" name="cpostal" required="required">
+        </p>
+        <p>
+            <label>Ciudad (*):</label>
+            <input type="text" name="ciudad" required="required">
+        </p>
+        <p>
+            <label>Provincia (*):</label>
+            <input type="text" name="provincia" required="required">
+        </p>
+        <p>
+            <input type="submit" value="Expediente" name="expediente">
+        </p>
+    </form>
+<?php
+}
+?>
+</body>
+</html>
--- clases/miHospital.php
+++ clases/miHospital.php
@@ -8,6 +8,25 @@ class miHospital
 		$this->conex = miConexion::conexion_instancia();
     }
 
+    public function comprobarDoctor($nif, $clave){
+        try {
+            $sql="SELECT * FROM `doctores` where DNI=? and PASSWORD=?";
+            $consulta=$this->conex->prepare($sql);
+            $consulta->bindParam(1, $nif);
+            $consulta->bindParam(2, $clave);
+            $consulta->execute();
+            if ($consulta->rowCount()>0){
+                $_SESSION["dni"]=$nif;
+                header("Location: ./bienvenida.php");
+            } else {
+                echo "<p class='error'>El usuario o la contraseña introducido/a no es válida.</p>";
+            }
+        } catch (PDOException $e){
+            echo "<p>Consulta: <b>" . $sql . "</b></p>";
+            echo "<p class='error'>Error: " . $e->getMessage() . "</p>";
+        }
+    }
+
 
     public function registrarDoctor($dni, $password, $nombre, $apellidos, $correo, $direccion, $cpostal, $ciudad, $provincia) {
         try {
@@ -30,6 +49,32 @@ class miHospital
 
            echo "<p>" . $sql . "</p>";
            echo "<p>Doctor registrado con éxito</p>";
+        } catch (PDOException $e) {
+			echo "<p class='error'>Error: " . $e->getMessage() . "</p>";
+        }
+    }
+
+    public function altaPaciente($dni, $nombre, $apellidos, $correo, $direccion, $cpostal, $ciudad, $provincia, $descripcion, $severidad, $dniDoctor) {
+        try {
+            $sql = "INSERT INTO pacientes(DNI, NOMBRE, APELLIDOS, CORREO, DIRECCION, CPOSTAL, CIUDAD, PROVINCIA, EXPEDIENTE, SEVERIDAD, DNI_DOCTOR) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+
+            $consulta = $this->conex->prepare($sql);
+            $consulta->bindParam(1, $dni);
+            $consulta->bindParam(2, $nombre);
+            $consulta->bindParam(3, $apellidos);
+            $consulta->bindParam(4, $correo);
+            $consulta->bindParam(5, $direccion);
+            $consulta->bindParam(6, $cpostal);
+            $consulta->bindParam(7, $ciudad);
+            $consulta->bindParam(8, $provincia);
+            $consulta->bindParam(9, $descripcion);
+            $consulta->bindParam(10, $severidad);
+            $consulta->bindParam(11, $dniDoctor);
+
+            $consulta->execute();
+
+           echo "<p>" . $sql . "</p>";
+           echo "<p>Paciente registrado con éxito</p>";
         } catch (PDOException $e) {
 			echo "<p class='error'>Error: " . $e->getMessage() . "</p>";
         }
